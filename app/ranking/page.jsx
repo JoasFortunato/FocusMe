161bde89a84e040c3b6b6d1c41213b9bab813db1
Marianameelo <melomariana928@gmail.com>
2025-11-21 14:23:59 -0300

    Ajustando os icones e as posições

--- app/ranking/page.jsx
+++ app/ranking/page.jsx
@@ -11,8 +11,8 @@ export default function Ranking() {
     { nome: "Phellipe", Horas: 10, avatar: "/images/phellipe.png" },
     { nome: "Lele", Horas: 7, avatar: "/images/lele.png" },
     { nome: "Marcela", Horas: 21, avatar: "/images/marcela.png" },
-    { nome: "Vitinho", Horas: 7, avatar: "/images/Vitinho.png" },
-    { nome: "Carlos", Horas: 23, avatar: "/images/carlos.png" },
+    { nome: "Vitinho", Horas: 23, avatar: "/images/Vitinho.png" },
+    { nome: "Carlos", Horas: 5, avatar: "/images/carlos.png" },
   ];
 
   const ordem = usuarios.sort((a, b) => b.Horas - a.Horas);
@@ -24,79 +24,99 @@ export default function Ranking() {
       {/* NAVBAR FIXA */}
       <Navbar />
 
-      {/* PÁGINA FULL SCREEN E ABAIXO DA NAVBAR */}
       <div className="min-h-screen w-full bg-[#0F0F0F] pt-20 flex items-center justify-center">
         <div className="flex items-center justify-center w-full">
           {/* CARD PRINCIPAL */}
-          <div className="bg-[#F6F6F6] rounded-2xl rounded-b-none relative flex flex-col items-center w-[45rem] h-[35rem] pt-40">
+          <div className="bg-[#F6F6F6] rounded-2xl rounded-b-none relative flex flex-col items-center w-[45rem] h-[32rem] mt-20">
             {/* PÓDIO */}
-            <div className="flex items-end gap-6 -mt-36">
+            <div className="flex items-end gap-6 mt-8">
               {/* SEGUNDO */}
               <div className="h-[11rem] w-[8rem] bg-[#8B5CF6] rounded-lg flex flex-col items-center justify-between p-3 hover:scale-110 transition">
-                <Image src={segundo.avatar} width={40} height={40} alt="" />
-                <p className="text-white text-[12px] -mt-7">{segundo.nome}</p>
-                <p className="text-white bg-[#7C3AED] rounded px-2 text-[10px] flex items-center gap-1 -mt-6">
+                <Image
+                  src={segundo.avatar}
+                  width={40}
+                  height={40}
+                  alt="Icone avatar"
+                />
+                <p className="text-white text-[12px] font-semibold -mt-5">
+                  {segundo.nome}
+                </p>
+                <p className="text-white bg-[#7C3AED] rounded px-2 text-[10px] flex items-center gap-1 -mt-4">
                   {segundo.Horas}H
                   <Image
                     src="/images/estrela.png"
                     width={10}
                     height={10}
-                    alt=""
+                    alt="Icone de estrela"
                   />
                 </p>
                 <Image
                   src="/images/trofeudois.png"
                   width={60}
                   height={60}
-                  alt=""
+                  alt="Trofeu segundo lugar"
                 />
               </div>
 
               {/* PRIMEIRO */}
               <div className="h-[13rem] w-[8rem] bg-[#8B5CF6] rounded-lg flex flex-col items-center justify-between p-3 hover:scale-110 transition">
-                <Image src={primeiro.avatar} width={40} height={40} alt="" />
-                <p className="text-white text-[12px] -m-20">{primeiro.nome}</p>
-                <p className="text-white bg-[#7C3AED] rounded px-2 text-[10px] flex items-center gap-1 mt-1">
+                <Image
+                  src={primeiro.avatar}
+                  width={40}
+                  height={40}
+                  alt="Icone avatar"
+                />
+                <p className="text-white text-[12px] -mt-4 font-semibold">
+                  {primeiro.nome}
+                </p>
+                <p className="text-white bg-[#7C3AED] rounded px-2 text-[10px] flex items-center gap-1 -mt-2 ">
                   {primeiro.Horas}H
                   <Image
                     src="/images/estrela.png"
                     width={10}
                     height={10}
-                    alt=""
+                    alt=" Icone de estrela"
                   />
                 </p>
                 <Image
                   src="/images/primeirolugar.png"
                   width={78}
                   height={78}
-                  alt=""
+                  alt="Trofeu primeiro lugar"
                 />
               </div>
 
               {/* TERCEIRO */}
               <div className="h-[10rem] w-[8rem] bg-[#8B5CF6] rounded-lg flex flex-col items-center justify-between p-3 hover:scale-110 transition">
-                <Image src={terceiro.avatar} width={33} height={33} alt="" />
-                <p className="text-white text-[12px] -mt-3">{terceiro.nome}</p>
+                <Image
+                  src={terceiro.avatar}
+                  width={33}
+                  height={33}
+                  alt="Icone avatar"
+                />
+                <p className="text-white font-semibold text-[12px] -mt-3">
+                  {terceiro.nome}
+                </p>
                 <p className="text-white bg-[#7C3AED] rounded px-2 text-[10px] flex items-center gap-1 -mt-2">
                   {terceiro.Horas}H
                   <Image
                     src="/images/estrela.png"
                     width={10}
                     height={10}
-                    alt=""
+                    alt="Icone de estrela"
                   />
                 </p>
                 <Image
                   src="/images/terceiro.png"
                   width={60}
                   height={50}
-                  alt=""
+                  alt="Trofeu terceiro lugar"
                 />
               </div>
             </div>
 
             {/* TÍTULO */}
-            <div className="h-[2rem] p-1 absolute -top-6 bg-[#F6F6F6] w-[13rem] flex justify-center rounded-2xl shadow-md">
+            <div className="h-[2rem] p-1 absolute -top-5 bg-[#F6F6F6] w-[13rem] flex justify-center rounded-2xl shadow-md">
               <h1 className="text-center font-semibold">RANKING BOARD</h1>
             </div>
 
