99ba80769b13019f9dfd26e84d7c48f2acb7fcaa
Marianameelo <melomariana928@gmail.com>
2025-11-24 20:07:41 -0300

    Fazendo alguns ajustes

--- app/Foto/page.jsx
+++ app/Foto/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function FotoPage() {
   const [username, setUsername] = useState("");
@@ -9,16 +10,10 @@ export default function FotoPage() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-[#7B2CF3]">
       <div className="bg-white w-[430px] rounded-2xl shadow-xl p-6 flex flex-col items-center">
-
         <h1 className="text-xl font-bold mb-4">Sua melhor foto aqui:</h1>
 
         <div className="w-[300px] h-[300px] bg-gray-300 rounded-full flex items-center justify-center border">
-          <Image
-            src="/images/gostoso.png"
-            width={70}
-            height={70}
-            alt="foto"
-          />
+          <Image src="/images/camera.png" width={70} height={70} alt="foto" />
         </div>
 
         <input
@@ -33,9 +28,10 @@ export default function FotoPage() {
           onChange={(e) => setUsername(e.target.value)}
         />
 
-        <button
-          className="
-            w-[80%] mt-5 
+        <Link href="./" className="w-full block">
+          <button
+            className="
+            w-[80%] mt-5 ml-10
             bg-[#7B2CF3] 
             text-white 
             py-3 
@@ -44,10 +40,10 @@ export default function FotoPage() {
             transition 
             hover:bg-blue-500
           "
-        >
-          Entrar
-        </button>
-
+          >
+            Entrar
+          </button>
+        </Link>
       </div>
     </div>
   );
